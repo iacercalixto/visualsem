ce7aac58a1703c1fb2618cc97708087f6157166a
Iacer Calixto <ic1179@nyu.edu>
2021-06-22 15:12:54 -0400

    Added files.

--- combine_sentencebert_glosses.py
+++ combine_sentencebert_glosses.py
@@ -5,35 +5,32 @@ from utils import load_bnids
 import h5py
 
 if __name__=="__main__":
-    strategies_list = ['all', 'all_but_swedish_and_farsi', 'top8']
+    visualsem_path = os.path.dirname(os.path.realpath(__file__))
+
+    strategies_list = ['all', 'top8']
     # For more details and discussion on VisualSem gloss quality/coverage, refer to: https://arxiv.org/pdf/2008.09150.pdf
     # We support the following strategies:
     # 'all': We simply use glosses in all languages concatenated as one large index.
-    # 'all_but_swedish_and_farsi': As the name suggests, we use all languages but Swedish and Farsi.
-    #       This is because the model we use (Sentence BERT) is not trained with sentences in these languages.
     # 'top8': We use the top-8 best-performing languages according to our experiments. These are: en, es, de, it, fr, pt, pl, nl.
     p = argparse.ArgumentParser()
+    p.add_argument('--visualsem_path', type=str, default=visualsem_path)
     p.add_argument('--strategy', default='top8', choices=strategies_list, type=str,
             help="""For more details and discussion on VisualSem gloss quality/coverage, refer to: https://arxiv.org/pdf/2008.09150.pdf.
                     We support the following strategies:
                     'all': We simply use glosses in all languages concatenated as one large index.
-                    'all_but_swedish_and_farsi': As the name suggests, we use all languages but Swedish and Farsi.
-                        This is because the model we use (Sentence BERT) is not trained with sentences in these languages.
                     'top8': We use the top-8 best-performing languages according to our experiments. These are: en, es, de, it, fr, pt, pl, nl.""")
     args = p.parse_args()
 
     if args.strategy == 'all':
         languages = ['ar', 'de', 'en', 'es', 'fr', 'it', 'ko', 'nl', 'pt', 'ru', 'zh', 'fa', 'pl', 'sv']
-    elif args.strategy == 'all_but_swedish_and_farsi':
-        languages = ['ar', 'de', 'en', 'es', 'fr', 'it', 'ko', 'nl', 'pt', 'ru', 'zh', 'pl']
     elif args.strategy == 'top8':
         languages = ['de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'pl']
     languages = sorted(languages)
-    gloss_bnids_fnames = ["glosses.%s.txt.bnids"%lang for lang in languages]
-    gloss_hdf5_fnames  = ["glosses.%s.txt.sentencebert.h5"%lang for lang in languages]
+    gloss_bnids_fnames = [os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.%s.txt.bnids"%lang) for lang in languages]
+    gloss_hdf5_fnames  = [os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.%s.txt.sentencebert.h5"%lang) for lang in languages]
 
     # concatenate BNids into single file
-    gloss_bnid_outfname = os.path.join("dataset", "gloss_files", "glosses.combined-%s.bnids"%args.strategy)
+    gloss_bnid_outfname = os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.combined-%s.bnids"%args.strategy)
     with open(gloss_bnid_outfname, 'w') as fh_out:
         n_lines = 0
         for gloss_bnid_fname in gloss_bnids_fnames:
@@ -44,17 +41,24 @@ if __name__=="__main__":
         print("Combined %i overall glosses into: %s"%(n_lines, gloss_bnid_outfname))
 
     # concatenate HDF5 features into single file
-    gloss_hdf5_outfname = os.path.join("dataset", "gloss_files", 'glosses.combined-%s.h5'%args.strategy)
+    gloss_hdf5_outfname = os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", 'glosses.combined-%s.h5'%args.strategy)
     with h5py.File(gloss_hdf5_outfname, 'w') as fh_out:
-        shape_features = (n_lines, 512)
-        fh_out.create_dataset("features", shape_features, dtype='float32', chunks=(1,512), maxshape=(None, 512), compression="gzip")
+        shape_features = (n_lines, 768)
+        shape_splits   = (n_lines, 1)
+        fh_out.create_dataset("language_idxs", shape_splits, dtype='uint8', chunks=(1,1), maxshape=(None, 1), compression="gzip")
+        fh_out.create_dataset("split_idxs", shape_splits, dtype='uint8', chunks=(1,1), maxshape=(None, 1), compression="gzip")
+        fh_out.create_dataset("features", shape_features, dtype='float32', chunks=(1,768), maxshape=(None, 768), compression="gzip")
 
         from_idx = 0
         for gloss_hdf5_fname in gloss_hdf5_fnames:
             with h5py.File(gloss_hdf5_fname, 'r') as fh_in:
-                gloss_feats = fh_in["features"][:]
+                gloss_langs  = fh_in["language_idxs"][:]
+                gloss_splits = fh_in["split_idxs"][:]
+                gloss_feats  = fh_in["features"][:]
                 to_idx = from_idx + gloss_feats.shape[0]
                 #gloss_feats = torch.tensor(gloss_feats)
+                fh_out['language_idxs'][from_idx:to_idx] = gloss_langs
+                fh_out['split_idxs'][from_idx:to_idx] = gloss_splits
                 fh_out['features'][from_idx:to_idx] = gloss_feats
             from_idx = to_idx
 
--- process_glosses_with_sentencebert.py
+++ process_glosses_with_sentencebert.py
@@ -1,6 +1,7 @@
 import torch
 import sys
 import os
+import random
 import json
 from collections import defaultdict
 import h5py
@@ -12,94 +13,73 @@ import argparse
 from utils import grouper, load_sentences, load_bnids, load_visualsem_bnids
 
 
-def evaluate_glosses(gloss_fname, bnid_fname, batch_size, all_gloss_sentences, all_gloss_bnids, all_bnids):
-    """
-        gloss_fname(str):                   Name of file containing glosses for a specific language.
-        bnid_fname(str):                    Name of file containing BNids (e.g. unique indices) for each gloss in `gloss_fname`.
-        batch_size(int):                    Batch size for Sentence BERT.
-        all_sentences(list[str]):           All glosses loaded from `gloss_fname`.
-        all_gloss_bnids(list[str]):         All gloss BNids loaded from `bnid_fname`.
-        all_bnids(list[str]):               All BNids/nodes in Visualsem.
-    """
-    # evaluate on random subset of 2000 glosses
-    valid_idxs = numpy.random.permutation( numpy.array( range(all_gloss_bnids.shape[0]) ) )[:2000]
-    n_examples = len(valid_idxs)
-    print("... Number of examples randomly selected for evaluation: ", n_examples)
-
-    with h5py.File(gloss_fname+".sentencebert.h5", 'r') as fh_gloss:
-        glosses_feats = fh_gloss["features"][:]
-        glosses_feats = torch.tensor(glosses_feats)
-        ranks_predicted = []
-        for idxs_ in grouper(batch_size, valid_idxs):
-            idxs = []
-            for i in idxs_:
-                if not i is None:
-                    idxs.append(i)
-
-            queries = glosses_feats[idxs]
-            scores = util.pytorch_cos_sim(queries, glosses_feats)
-            scores = scores.cpu().numpy()
-            # query vs. itself is in main diagonal. fill it with large negative not to retrieve it.
-            other_glosses_with_same_bnid_minibatch = []
-            for x,y in zip(list(range(batch_size)), idxs):
-                scores[x,y] = -10.0
-                # retrieve ground-truth bnids for each example
-                this_gloss_bnid = all_gloss_bnids[ y ]
-                other_glosses_with_same_bnid = numpy.where(all_gloss_bnids==this_gloss_bnid)[0]
-                other_glosses_with_same_bnid_minibatch.append( other_glosses_with_same_bnid )
-
-            ranks = numpy.argsort(scores) # sort scores by cosine similarity (low to high)
-            ranks = ranks[:,::-1] # sort by cosine similarity (high to low)
-            for r,index,bnid in zip(range(ranks.shape[0]), idxs, other_glosses_with_same_bnid_minibatch):
-                # uncomment the print statement below to debug/gather details on the predicted nodes.
-                #print(
-                #        "bnid: ", all_gloss_bnids[bnid[0]], ", bnid indices: ",  bnid,
-                #        ", numpy.in1d(ranks[r], bnid_indices): ",
-                #        numpy.in1d(ranks[r], bnid),
-                #        numpy.where( numpy.in1d(ranks[r], bnid) )[0],
-                #        "bnid rank 0: ", all_gloss_bnids[ranks[r,0]]
-                #)
-                rank_predicted = numpy.where( numpy.in1d(ranks[r], bnid) )[0][0]
-                ranks_predicted.append( rank_predicted )
-
-        ranks_predicted = numpy.array(ranks_predicted)
-        #print("ranks_predicted: ", ranks_predicted)
-        print("... Rank mean/std: ", ranks_predicted.mean().item(), ranks_predicted.std().item())
-        for k in [1,3,5,10]:
-            print("... Accuracy (hits@%i): %.2f%%"%(k, (ranks_predicted<=(k-1)).sum()*1.0/ranks_predicted.shape[0]*100))
-
-
-def compute_glosses(gloss_fname, bnid_fname, batch_size, all_sentences, all_bnids):
+def compute_glosses(gloss_fname, bnid_fname, batch_size, all_sentences, all_bnids, lang_idx):
     """
         gloss_fname(str):                   Name of file containing glosses for a specific language.
         bnid_fname(str):                    Name of file containing BNids (e.g. unique indices) for each gloss in `gloss_fname`.
         batch_size(int):                    Batch size for Sentence BERT.
         all_sentences(list[str]):           All glosses loaded from `gloss_fname`.
         all_bnids(list[str]):               All gloss BNids loaded from `bnid_fname`.
+        lang_idx(int):                      Language idx of the glosses (integer).
     """
     n_lines = len(all_sentences)
-    model = SentenceTransformer('distiluse-base-multilingual-cased')
-    shape_features = (n_lines, 512)
-    with h5py.File(gloss_fname+".sentencebert.h5", 'w') as fh_out:
-        fh_out.create_dataset("features", shape_features, dtype='float32', chunks=(1,512), maxshape=(None, 512), compression="gzip")
 
-        for from_idx in tqdm.trange(0,n_lines,batch_size):
+    # reserve 2,000 glosses for valid and 2,000 glosses for test set
+    assert(n_lines > 10000), "Gloss file has less than 10000 lines: %s"%gloss_fname
+    all_idxs = list(range(n_lines))
+    random.shuffle(all_idxs)
+    valid_idxs = all_idxs[:2000]
+    test_idxs  = all_idxs[2000:4000]
+    train_idxs = all_idxs[4000:]
+
+    #model = SentenceTransformer('distiluse-base-multilingual-cased')
+    model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
+    shape_features = (n_lines, 768)
+    shape_splits   = (n_lines, 1)
+    with h5py.File(gloss_fname+".sentencebert.h5", 'w') as fh_out:
+        fh_out.create_dataset("features", shape_features, dtype='float32', chunks=(1,768), maxshape=(None, 768), compression="gzip")
+        # train -> 0, valid -> 1, test-> 2
+        fh_out.create_dataset("split_idxs", shape_splits, dtype='uint8', chunks=(1,1), maxshape=(None, 1), compression="gzip")
+        # by running this script using the default languages, we have:
+        # ar -> 0, de -> 1, en -> 2, es -> 3, fr -> 4, it -> 5, ko -> 6, nl -> 7, pt -> 8, ru -> 9, zh -> 10, fa -> 11, pl -> 12, sv -> 13
+        fh_out.create_dataset("language_idxs", shape_splits, dtype='uint8', chunks=(1,1), maxshape=(None, 1), compression="gzip")
+
+        for from_idx in tqdm.trange(0, n_lines, batch_size):
             to_idx = from_idx+batch_size if from_idx+batch_size <= n_lines else n_lines
             #print(from_idx, to_idx)
             batch_sentences = all_sentences[ from_idx: to_idx ]
             emb_sentences = model.encode(batch_sentences, convert_to_tensor=True)
             #test_queries(emb_sentences, all_sentences, model)
+
+            split_idxs = []
+            for curr_idx in range(from_idx, to_idx):
+                if curr_idx in train_idxs:
+                    curr_idx = 0
+                elif curr_idx in valid_idxs:
+                    curr_idx = 1
+                elif curr_idx in test_idxs:
+                    curr_idx = 2
+                else:
+                    raise Exception()
+                split_idxs.append( curr_idx )
+
+            fh_out["split_idxs"][from_idx:to_idx] = numpy.array(split_idxs, dtype='uint8')[:,None]
             fh_out["features"][from_idx:to_idx] = emb_sentences.cpu().numpy()
+            fh_out["language_idxs"][from_idx:to_idx] = numpy.array([lang_idx]*(to_idx-from_idx))[:,None]
 
 
 if __name__=="__main__":
-    visualsem_path = "/misc/vlgscratch5/ChoGroup/icalixto/visualsem_krakatoa"
-    visualsem_nodes_path = "%s/dataset/nodes.json"%visualsem_path
+    visualsem_path = os.path.dirname(os.path.realpath(__file__))
+    visualsem_glosses_path = "%s/dataset/gloss_files/"%visualsem_path
+    visualsem_nodes_path = "%s/dataset/nodes.v2.json"%visualsem_path
     visualsem_images_path = "%s/dataset/images/"%visualsem_path
     visualsem_gloss_languages = ['ar', 'de', 'en', 'es', 'fr', 'it', 'ko', 'nl', 'pt', 'ru', 'zh', 'fa', 'pl', 'sv']
+    #visualsem_gloss_languages = ['en']
     p = argparse.ArgumentParser()
     p.add_argument('--visualsem_path', type=str, default=visualsem_path,
             help="Path to directory containing VisualSem knowledge graph.")
+    p.add_argument('--visualsem_glosses_path', type=str, default=visualsem_glosses_path,
+            help="Path to directory containing VisualSem glosses.")
     p.add_argument('--visualsem_nodes_path', type=str, default=visualsem_nodes_path,
             help="Path to file containing VisualSem nodes.")
     p.add_argument('--visualsem_images_path', type=str, default=visualsem_images_path,
@@ -112,10 +92,13 @@ if __name__=="__main__":
 
     # load all nodes in VisualSem
     all_bnids = load_visualsem_bnids(args.visualsem_nodes_path, args.visualsem_images_path)
-    gloss_fnames = ['%s/dataset/gloss_files/glosses.%s.txt'%(args.visualsem_path, l) for l in args.gloss_languages]
+    gloss_fnames = ['%s/glosses.%s.txt'%(args.visualsem_glosses_path, l) for l in args.gloss_languages]
     #batch_size = 1000
 
-    for gloss_fname in gloss_fnames:
+    for lang_idx, gloss_fname in enumerate(gloss_fnames):
+        #lang = args.gloss_languages[lang_idx]
+        #lang_idx = visualsem_gloss_languages.index(lang)
+
         # load all glosses for the language
         all_gloss_sentences = load_sentences( gloss_fname )
         # load all BNids for each gloss
@@ -123,12 +106,15 @@ if __name__=="__main__":
         all_gloss_bnids = load_bnids( bnid_fname )
         all_gloss_bnids = numpy.array(all_gloss_bnids, dtype='object')
 
-        if not os.path.isfile(gloss_fname+".sentencebert.h5"):
-            # if it still has not been created, create the gloss index using Sentence BERT.
-            # https://github.com/UKPLab/sentence-transformers
-            compute_glosses(gloss_fname, bnid_fname, args.batch_size, all_gloss_sentences, all_gloss_bnids)
+        assert(not os.path.isfile(gloss_fname+".sentencebert.h5")), \
+                "File found: '%s'. Please remove it manually to avoid tampering."%gloss_fname+".sentencebert.h5"
 
-        else:
-            # randomly select a subset of the glosses for the language and evaluate node retrieval.
-            evaluate_glosses(gloss_fname, bnid_fname, args.batch_size, all_gloss_sentences, all_gloss_bnids, all_bnids)
+        #if not os.path.isfile(gloss_fname+".sentencebert.h5"):
+        # if it still has not been created, create the gloss index using Sentence BERT.
+        # https://github.com/UKPLab/sentence-transformers
+        compute_glosses(gloss_fname, bnid_fname, args.batch_size, all_gloss_sentences, all_gloss_bnids, lang_idx)
+
+        #else:
+        #    # randomly select a subset of the glosses for the language and evaluate node retrieval.
+        #    evaluate_glosses(gloss_fname, bnid_fname, args.batch_size, all_gloss_sentences, all_gloss_bnids, all_bnids)
 
