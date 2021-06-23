27403de2d1369d53b6a94e64f4be0d1e962624c0
Iacer Calixto <iacercalixto@gmail.com>
2021-06-23 10:25:06 +0200

    Fixed path to glosses in v2.

--- combine_sentencebert_glosses.py
+++ combine_sentencebert_glosses.py
@@ -5,7 +5,7 @@ from utils import load_bnids
 import h5py
 
 if __name__=="__main__":
-    visualsem_path = os.path.dirname(os.path.realpath(__file__))
+    path_to_glosses = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dataset", "gloss_files")
 
     strategies_list = ['all', 'top8']
     # For more details and discussion on VisualSem gloss quality/coverage, refer to: https://arxiv.org/pdf/2008.09150.pdf
@@ -13,7 +13,8 @@ if __name__=="__main__":
     # 'all': We simply use glosses in all languages concatenated as one large index.
     # 'top8': We use the top-8 best-performing languages according to our experiments. These are: en, es, de, it, fr, pt, pl, nl.
     p = argparse.ArgumentParser()
-    p.add_argument('--visualsem_path', type=str, default=visualsem_path)
+    p.add_argument('--path_to_glosses', type=str, default=path_to_glosses,
+            help="""Path to the directory with the gloss files.""")
     p.add_argument('--strategy', default='top8', choices=strategies_list, type=str,
             help="""For more details and discussion on VisualSem gloss quality/coverage, refer to: https://arxiv.org/pdf/2008.09150.pdf.
                     We support the following strategies:
@@ -26,11 +27,11 @@ if __name__=="__main__":
     elif args.strategy == 'top8':
         languages = ['de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'pl']
     languages = sorted(languages)
-    gloss_bnids_fnames = [os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.%s.txt.bnids"%lang) for lang in languages]
-    gloss_hdf5_fnames  = [os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.%s.txt.sentencebert.h5"%lang) for lang in languages]
+    gloss_bnids_fnames = [os.path.join(args.path_to_glosses, "glosses.%s.txt.bnids"%lang) for lang in languages]
+    gloss_hdf5_fnames  = [os.path.join(args.path_to_glosses, "glosses.%s.txt.sentencebert.h5"%lang) for lang in languages]
 
     # concatenate BNids into single file
-    gloss_bnid_outfname = os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", "glosses.combined-%s.bnids"%args.strategy)
+    gloss_bnid_outfname = os.path.join(args.path_to_glosses, "glosses.combined-%s.bnids"%args.strategy)
     with open(gloss_bnid_outfname, 'w') as fh_out:
         n_lines = 0
         for gloss_bnid_fname in gloss_bnids_fnames:
@@ -41,7 +42,7 @@ if __name__=="__main__":
         print("Combined %i overall glosses into: %s"%(n_lines, gloss_bnid_outfname))
 
     # concatenate HDF5 features into single file
-    gloss_hdf5_outfname = os.path.join(args.visualsem_path, "dataset", "gloss_files_v2", 'glosses.combined-%s.h5'%args.strategy)
+    gloss_hdf5_outfname = os.path.join(args.path_to_glosses, 'glosses.combined-%s.h5'%args.strategy)
     with h5py.File(gloss_hdf5_outfname, 'w') as fh_out:
         shape_features = (n_lines, 768)
         shape_splits   = (n_lines, 1)
